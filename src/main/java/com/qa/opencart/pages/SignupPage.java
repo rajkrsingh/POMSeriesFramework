4f4e91e33fe5853897f55234b812da14448fb316
singh-ext, Shambu (GDS-SWOI) <Shambu.singh-ext@bshg.com>
2022-03-20 20:01:18 +0530

    modified signup page

--- src/main/java/com/qa/opencart/pages/SignupPage.java
+++ src/main/java/com/qa/opencart/pages/SignupPage.java
@@ -1,9 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class SignupPage {
+	
+	private By registration=By.id("registration");
 	
 	public SignupPage() {
 		System.out.println("This is sigup page constructor....");
+	}
+	
+	public void doRegisrationPage() {
+		System.out.println("This is registration page....");
 	}
 
 }
