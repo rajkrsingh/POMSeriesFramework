472da9e90886a62a5524cae87ba3e85c85ea8597
unknown <rajkumasingh@gmail.com>
2022-07-30 17:27:11 +0530

    updated signup page

--- src/main/java/com/qa/opencart/pages/SignupPage.java
+++ src/main/java/com/qa/opencart/pages/SignupPage.java
@@ -8,10 +8,11 @@ public class SignupPage {
 	
 	public SignupPage() {
 		System.out.println("This is sigup page constructor....");
-	}
+	}	
 	
 	public void doRegisrationPage() {
 		System.out.println("This is registration page....");
+		System.out.println("Practicing master and signup branch concept");
 	}
 
 }
